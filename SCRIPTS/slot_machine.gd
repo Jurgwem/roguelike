679cef14bba05785b4a8d68d8f46da6b1e42804e
Jurgwem <n.bresi05@gmail.com>
2025-01-21 21:59:07 +0100

    tweaked slot machine

--- SCRIPTS/slot_machine.gd
+++ SCRIPTS/slot_machine.gd
@@ -3,7 +3,7 @@ extends Node2D
 @onready var gm : Node2D = get_node("/root/game/gameManager");
 
 var used : bool = false;
-var winChance : float = randf();
+var winChance = 0.0;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,8 +14,9 @@ func _process(delta: float) -> void:
 	pass
 	
 func play() -> void:
+	winChance = randf();
 	$AnimatedSprite2D.play("gamble");
-	await get_tree().create_timer(3 + (randf() * 3)).timeout;
+	await get_tree().create_timer(4 + (randf() * 3)).timeout;
 	if winChance < 0.5:
 		$AnimatedSprite2D.animation = "win";
 		$winPart.emitting = true;
@@ -24,6 +25,9 @@ func play() -> void:
 		$AnimatedSprite2D.animation = "lose";
 		$losePart.emitting = true;
 		gm.coins *= 0.5;
+	await get_tree().create_timer(2).timeout;
+	$AnimatedSprite2D.animation = "idle";
+	used = false;
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "player" and !used:
